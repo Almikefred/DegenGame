3e5284768893e834ddb13ae0fd4aef457fd5ce97
Almikefred <alfredmichael494@gmail.com>
2024-08-15 10:07:56 +0100

    Update DagenToken.sol

--- contracts/DagenToken.sol
+++ contracts/DagenToken.sol
@@ -8,17 +8,28 @@ import "hardhat/console.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
+    struct StoreItem {
+        string name;
+        uint256 cost;
+    }
+
     struct StoreRewards {
         uint DegenSword;
         uint DegenNFT;
         uint DegenShield;
     }
-    mapping (address => StoreRewards) public redeemed;
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+    mapping(uint8 => StoreItem) public storeItems;
+    mapping(address => StoreRewards) public redeemed;
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        storeItems[1] = StoreItem("Degen Sword", 100);
+        storeItems[2] = StoreItem("Degen NFT", 75);
+        storeItems[3] = StoreItem("Degen Shield", 50);
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount); // last value is for decimals
+        _mint(to, amount);
     }
 
     function decimals() public pure override returns (uint8) {
@@ -46,49 +57,55 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
         burn(_value);
     }
 
-    function showStoreItems() external pure returns (string memory) {
-        console.log("The following items are available for purchase:");
-        console.log("Selection 1.  DegenSword");
-        console.log("Selection 2. Degen Swag");
-        console.log("Selection 3. Degen Shield");
+    function showStoreItems() external view returns (string memory) {
         return
-            "The following items are available for purchase:\nSelection 1.  DegenSword\nSelection 2.  Degen NFT\nSelection 3.  Degen Shield ";
+            string(abi.encodePacked(
+                "The following items are available for purchase:\n",
+                "Selection 1. ", storeItems[1].name, " (Cost: ", uint2str(storeItems[1].cost), " DGN)\n",
+                "Selection 2. ", storeItems[2].name, " (Cost: ", uint2str(storeItems[2].cost), " DGN)\n",
+                "Selection 3. ", storeItems[3].name, " (Cost: ", uint2str(storeItems[3].cost), " DGN)"
+            ));
     }
 
-    function redeemTokens(uint8 _userChoice) external payable returns (bool) {
+    function redeemTokens(uint8 _userChoice) external returns (bool) {
+        require(_userChoice >= 1 && _userChoice <= 3, "Invalid choice");
+
+        StoreItem memory item = storeItems[_userChoice];
+        require(balanceOf(msg.sender) >= item.cost, "You do not have enough Degen Tokens");
+
         StoreRewards storage redeemedItems = redeemed[msg.sender];
         if (_userChoice == 1) {
-            require(
-                this.balanceOf(msg.sender) >= 100,
-                "You do not have enough Degen Tokens"
-            );
             redeemedItems.DegenSword++;
-            burn(100);
-            console.log("You have redeemed for a Degen Sword!");
-            return true;
         } else if (_userChoice == 2) {
-            require(
-                this.balanceOf(msg.sender) >= 75,
-                "You do not have enough Degen Tokens"
-            );
-           redeemedItems.DegenNFT++;
-            burn(75);
-            console.log("You have redeemed for an official Degen NFT");
-            return true;
+            redeemedItems.DegenNFT++;
         } else if (_userChoice == 3) {
-            require(
-                this.balanceOf(msg.sender) >= 50,
-                "You do not have enough Degen Tokens"
-            );
-           redeemedItems.DegenShield++;
-             burn(50);
-            console.log(
-                "You have redeemed for OG status in our Degen Discord!"
-            );
-            return true;
-        } else {
-            console.log("That is not a valid choice");
-            return false;
+            redeemedItems.DegenShield++;
         }
+
+        burn(item.cost);
+        console.log("You have redeemed for a %s!", item.name);
+        return true;
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
